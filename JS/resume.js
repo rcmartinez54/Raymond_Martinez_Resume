49f89644c467f2cf8b70cfc459ecdc308da36d95
Jake Howard <bsidedrums@Jake-Howards-MacBook-Pro.local>
2018-07-24 19:00:02 -0700

    add

--- JS/resume.js
+++ JS/resume.js
@@ -1,94 +1,77 @@
 
-
-// // Select all links with hashes
-// $('a[href*="#"]')
-//   // Remove links that don't actually link to anything
-//   .not('[href="#"]')
-//   .not('[href="#0"]')
-//   .click(function(event) {
-//     // On-page links
-//     if (
-//       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-//       && 
-//       location.hostname == this.hostname
-//     ) {
-//       // Figure out element to scroll to
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       // Does a scroll target exist?
-//       if (target.length) {
-//         // Only prevent default if animation is actually gonna happen
-//         event.preventDefault();
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 2000, function() {
-//           // Callback after animation
-//           // Must change focus!
-//           var $target = $(target);
-//           $target.focus();
-//           if ($target.is(":focus")) { // Checking if the target was focused
-//             return false;
-//           } else {
-//             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-//             $target.focus(); // Set focus again
-//           };
-//         });
-//       }
-//     }
-//   });
-
-
-$('a[href^="#"]').on('click', function(event) {
-
-    var target = $(this.getAttribute('href'));
-
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-
-});
-
-
-$('ul').find('a').click(function(){
-    var $href = $(this).attr('href');
-    var $anchor = $('#'+$href).offset();
-	$('body').animate({ scrollTop: $anchor.top }, 3000);
-    return false;
-});
-
-
 $(document).ready(function() {
-	$('#previous').on('click', function() {
-		$('#im_' + currentImage).stop().fadeOut(1);
-		decreaseImage();
-		$('#im_' + currentImage).stop().fadeIn(1);
+
+	// function showHeadline() {
+	// 	$('.intro h1').fadeIn(500, function () {
+	// 		$('.intro h2').delay(1000).fadeIn(500);
+	// 	});
+	// };
+
+	// showHeadline();
+
+	(function () {
+		$('.intro h1').fadeIn(500, function () {
+			$('.intro h2').delay(1000).fadeIn(500);
+		});
+	})();
+
+	// $('.intro h1').hide();
+	// $('.intro h1').fadeIn(3000);
+
+	// $('.intro h2').hide();
+	// $('.intro h2').fadeIn(8000);
+
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $(this.getAttribute('href'));
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').stop().animate({
+	            scrollTop: target.offset().top
+	        }, 1000);
+	    }
+
 	});
-	$('#next').on('click', function() {
-		$('#im_' + currentImage).stop().fadeOut(1);
-		increaseImage();
-		$('#im_' + currentImage).stop().fadeIn(1);
+
+
+	$('ul').find('a').click(function(){
+	    var $href = $(this).attr('href');
+	    var $anchor = $('#'+$href).offset();
+		$('body').animate({ scrollTop: $anchor.top }, 3000);
+	    return false;
 	});
 
-	var currentImage = 1;
-	var totalImages = 5;
 
-	function increaseImage() {
-		++currentImage;
-		if(currentImage > totalImages) {
-			currentImage = 1;
+	$(document).ready(function() {
+		$('#previous').on('click', function() {
+			$('#im_' + currentImage).stop().fadeOut(1);
+			decreaseImage();
+			$('#im_' + currentImage).stop().fadeIn(1);
+		});
+		$('#next').on('click', function() {
+			$('#im_' + currentImage).stop().fadeOut(1);
+			increaseImage();
+			$('#im_' + currentImage).stop().fadeIn(1);
+		});
+
+		var currentImage = 1;
+		var totalImages = 5;
+
+		function increaseImage() {
+			++currentImage;
+			if(currentImage > totalImages) {
+				currentImage = 1;
+			}
 		}
-	}
 
-	function decreaseImage() {
-		--currentImage;
-		if(currentImage < 1) {
-			currentImage = totalImages;
+		function decreaseImage() {
+			--currentImage;
+			if(currentImage < 1) {
+				currentImage = totalImages;
+			}
 		}
-	}
-});
-
+	});
+}); //End ready
 
 
